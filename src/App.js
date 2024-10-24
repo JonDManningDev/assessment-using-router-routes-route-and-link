0eb98f9adf4e762692f3d58435d6da54d1253c52
Jonathan <jondmanning1986@gmail.com>
2024-10-24 14:06:38 -0400

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    // No need to add <Router>, it has been added to ./index.js
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the home page</h1>} />
+        <Route path="/about" element={<h1>You are on the about page</h1>} />
+        <Route path="/contact" element={<h1>Please feel free to email us</h1>} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
     </div>
   );
 }
